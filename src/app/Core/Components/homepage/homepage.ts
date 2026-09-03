3f1673f92363de529c137983d4209c1fc1055902
infonizamk-crypto <info.nizamk@gmail.com>
2026-09-03 03:16:24 +0300

    Fix template and style URLs in Homepage component

--- src/app/Core/Components/homepage/homepage.ts
+++ src/app/Core/Components/homepage/homepage.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-Homepage',
   imports: [],
-  templateUrl: './Homepage.html',
-  styleUrl: './Homepage.css',
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css',
 })
 export class Homepage {
   
